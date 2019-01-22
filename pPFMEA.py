9d541116ab5628af7b705a37e1ed0689ff254aa0
codylruff <39601024+codylruff@users.noreply.github.com>
2019-01-21 23:20:28 -0500

    Create pPFMEA.py

---
+++ pPFMEA.py
@@ -0,0 +1,14 @@
+class FMEA(object):
+    
+    def __init__(self, name):
+        self.name = name
+        self.lines = []
+        
+    def add_line(newLine):
+        self.lines.add(newLine)
+        
+class line(object):
+
+    def __init__(self, failureMode, severity, occurances, detectability):
+        self.failureMode = failureMode
+        
