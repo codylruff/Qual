d88491e9b889c8b0c89b0065e8ef02f45be4c718
Cody Ruff <CRuff@saatigroup.net>
2019-03-05 16:17:47 -0500

    This actually works now.

--- imgComp.py
+++ imgComp.py
@@ -1,12 +1,43 @@
 from PIL import Image
+import glob
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-def compressImage(path, reduce_by):
 
+def compressImage(folder, path, reduce_by):
     image = Image.open(path)
     size = image.size
 
-    size(0) = size(0) * reduce_by
-    size(1) = size(1) * reduce_by
+    new_size = (int(size[0] * reduce_by), int(size[1] * reduce_by))
 
-    image = image.resize(size,Image.ANTIALIAS)
-    image.save(path,optimize=True, quality=95)
+    image = image.resize(new_size, Image.ANTIALIAS)
+    new_path = path.replace(folder, folder + '/Compressed Images')
+    final_path = new_path.replace('.JPG', '_Compressed.JPG')
+    print(final_path)
+    image.save(final_path, optimize=True, quality=95)
+
+def main(*args, **kwargs):
+
+    root = tk.Tk()
+    root.withdraw()
+
+    folder_selected = filedialog.askdirectory()
+    images_dir = folder_selected + '\*.JPG'
+    print(images_dir)
+    images = glob.glob(images_dir)
+
+    new_folder = folder_selected + '\Compressed Images'
+    try:
+        os.mkdir(new_folder)
+    except OSError:
+        print("Creation of the directory %s failed" % new_folder)
+    else:
+        print("Successfully created the directory %s " % new_folder)
+
+    for i in range(0, len(images)):
+        compressImage(folder_selected, images[i], 0.25)
+    return 0
+
+if __name__ == "__main__":
+    main()
